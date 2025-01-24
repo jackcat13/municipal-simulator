13283d61c3edb33e5f28533541b0b2ca52fea3a1
jackcat13 <christophehenry11@gmail.com>
2025-01-24 20:43:49 +0100

    feat: Make drawing in window

--- municipal-simulator.asm
+++ municipal-simulator.asm
@@ -1,19 +1,39 @@
 format ELF64
 
 section '.text' executable
-public _start
+public main
+extrn BeginDrawing
+extrn ClearBackground
+extrn CloseWindow
+extrn EndDrawing
+extrn _exit
 extrn InitWindow
+extrn WindowShouldClose
 
-_start:
+main:
     mov rdi, 800
     mov rsi, 600
     mov rdx, title
     call InitWindow
-    
-_loop:
-    jmp _loop
 
+    .not_closed_window:
+        call WindowShouldClose
+        test rax, rax
+        jnz .closed_window
+
+        call BeginDrawing
+        mov rdi, 0xFF181818
+        call ClearBackground
+        call EndDrawing
+        
+        jmp .not_closed_window
+    
+    .closed_window:
+        call CloseWindow
+        mov rdi, 0
+        call _exit
 
 section '.data' writeable
 title: db "Municipal simulator", 0
+
 section '.note.GNU-stack'
